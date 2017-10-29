8f17b13d6eaf604f93062baab65a0a7eb8ebe6fd
Enzo Cioppettini <tenstrings5050@gmail.com>
2017-10-29 02:15:23 -0300

    Él había hecho como 7 tests, no estoy seguro de para qué eran

--- 09-TusLibros.com/Python/xxx.py
+++ 09-TusLibros.com/Python/xxx.py
@@ -1,39 +1,73 @@
-#For python this file uses encoding: utf-8
+# For python this file uses encoding: utf-8
 import unittest
 from collections import defaultdict
 
+
 class ShoppingCart(object):
-	
-	def __init__(self,*args,**kwargs):
-		self.copies_by_title = defaultdict(int)
 
-	def is_empty(self):
-		return len(self.copies_by_title) == 0
+    NOT_IN_CATALOG_ERROR_MSG = "Item's not in catalog"
 
-	def add_title(self,a_title,copies):
-		self.copies_by_title[a_title] += copies 
-	
-	def content(self):
-		return self.copies_by_title
+    def __init__(self, a_catalog):
+        self.items = defaultdict(int)
+        self.catalog = a_catalog
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def add(self, item, quantity):
+        if item in self.catalog:
+            self.items[item] += quantity
+        else:
+            raise Exception(self.__class__.NOT_IN_CATALOG_ERROR_MSG)
+
+    def number_of(self, an_item):
+        return self.items[an_item]
+
 
 class Catalog(object):
-	pass
+    def __init__(self, *args, **kwargs):
+        self._items = set()
 
+    def __contains__(self, an_item):
+        return an_item in self._items
+
+    def add_item(self, item):
+        self._items.add(item)
+
+
+# TODO: Creo que habría que cambiarle el nombre a esta clase
 class ShoppingCartTest(unittest.TestCase):
-	
-	def setUp(self):
-		self.cart = ShoppingCart()
 
-	def test01_new_cart_is_empty(self):
-		self.assertTrue(self.cart.is_empty())
+    def setUp(self):
+        self.item_in_catalog = object()
+        self.item_not_in_catalog = object()
+        catalog = Catalog()
+        catalog.add_item(self.item_in_catalog)
+        self.cart = ShoppingCart(catalog)
 
-	def test02_may_add_title_copies_to_cart(self):
-		a_title = object()
-		self.cart.add_title(a_title,10)
-		self.assertTrue( self.cart.content()[a_title] >= 10 )
+    def test01_new_cart_is_empty(self):
+        self.assertTrue(self.cart.is_empty())
 
-	def test03_xxx(self):
-		pass
+    def test02_may_add_copies_of_item_in_catalog_to_cart(self):
+        item_to_add = self.item_in_catalog
+        self.cart.add(item_to_add, 10)
+        self.assertTrue(self.cart.number_of(item_to_add) >= 10)
+
+    def test03_can_not_add_items_that_are_not_in_the_catalog(self):
+        with self.assertRaises(Exception) as cm:
+            self.cart.add(self.item_not_in_catalog, 1)
+
+        self.assertEqual(
+            cm.exception.message,
+            ShoppingCart.NOT_IN_CATALOG_ERROR_MSG
+        )
+
+        # No estoy seguro de si esta es la mejor manera de chequearlo...
+        # O más bien, no estoy seguro de que esto no tenga que
+        # tirar una excepción
+
+        self.assertEqual(self.cart.number_of(self.item_not_in_catalog), 0)
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
