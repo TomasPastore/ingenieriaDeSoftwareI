125606642f93bcc8135dede526fa00fbe830b223
Enzo Cioppettini <tenstrings5050@gmail.com>
2017-10-29 03:43:16 -0300

    Ni idea de si estará bien, pero algo es algo xD

--- 09-TusLibros.com/Python/xxx.py
+++ 09-TusLibros.com/Python/xxx.py
@@ -1,6 +1,7 @@
 # For python this file uses encoding: utf-8
 import unittest
 from collections import defaultdict
+from datetime import date, timedelta
 
 
 class ShoppingCart(object):
@@ -26,17 +27,42 @@ class ShoppingCart(object):
 
 class Catalog(object):
     def __init__(self, *args, **kwargs):
+        # Esto no es inmutable, así que yo le pasaría items iniciales...
         self._items = set()
 
     def __contains__(self, an_item):
         return an_item in self._items
 
-    def add_item(self, item):
+    def add(self, item):
         self._items.add(item)
 
 
+class CheckoutError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class CreditCard(object):
+    def __init__(self, number, expiration_date, card_owner):
+        self.number = number
+        self.expiration_date = expiration_date
+        self.card_owner = card_owner
+
+
 class Cashier(object):
-    pass
+    EXPIRATED_CARD_ERROR_MSG = 'This card is no longer valid'
+    EMPTY_CART_ERROR_MESSAGE = 'Can not check out an empty cart'
+
+    def check_out(self, cart, credit_card):
+        # < o <=????
+        if credit_card.expiration_date < date.today():
+            raise CheckoutError(
+                self.__class__.EXPIRATED_CARD_ERROR_MSG
+                )
+        if cart.is_empty:
+            raise CheckoutError(
+                self.__class__.EMPTY_CART_ERROR_MESSAGE
+            )
 
 
 # TODO: Creo que habría que cambiarle el nombre a esta clase
@@ -46,7 +72,7 @@ class ShoppingCartTest(unittest.TestCase):
         self.item_in_catalog = object()
         self.item_not_in_catalog = object()
         catalog = Catalog()
-        catalog.add_item(self.item_in_catalog)
+        catalog.add(self.item_in_catalog)
         self.cart = ShoppingCart(catalog)
 
     def test01_new_cart_is_empty(self):
@@ -75,8 +101,46 @@ class ShoppingCartTest(unittest.TestCase):
 
 class CashierTests(unittest.TestCase):
 
-    def test01_xxx(self):
-        pass
+    def setUp(self):
+        catalog = Catalog()
+        item = object()
+        catalog.add(item)
+        self.empty_cart = ShoppingCart(catalog)
+        self.non_empty_cart = ShoppingCart(catalog)
+
+        self.non_empty_cart.add(item, 1)
+
+        self.cashier = Cashier()
+        self.expirated_card = CreditCard(
+            number=22,
+            expiration_date=date.today() - timedelta(days=1),
+            card_owner='Pedro de Mendoza'
+        )
+        self.valid_card = CreditCard(
+            number=23,
+            expiration_date=date.today() + timedelta(days=1),
+            card_owner='Gilgamesh'
+        )
+
+    def test01_can_not_checkout_empty_cart(self):
+        with self.assertRaises(CheckoutError) as cm:
+            self.cashier.check_out(self.empty_cart, self.valid_card)
+
+        self.assertEqual(
+            cm.exception.message,
+            Cashier.EMPTY_CART_ERROR_MESSAGE,
+        )
+
+    def test02_cashier_can_not_checkout_with_invalid_card(self):
+        with self.assertRaises(CheckoutError) as cm:
+            self.cashier.check_out(self.non_empty_cart, self.expirated_card)
+
+        self.assertEqual(
+            cm.exception.message,
+            Cashier.EXPIRATED_CARD_ERROR_MSG
+        )
+
+        self.assertFalse(self.non_empty_cart.is_empty())
 
 
 if __name__ == '__main__':
